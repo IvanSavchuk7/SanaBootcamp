a7e48a224d558ea845093a99795f47debb6db772
Ivan Savchuk <ipzm222_sio@student.ztu.edu.ua>
2023-03-25 00:33:43 +0200

    MVC app Small Task done

---
+++ HelloWebApp/HelloWebApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using HelloWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HelloWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/")]
+        public IActionResult SayHello(UserViewModel user)
+        {
+            if (user.Name == null)
+                return View(nameof(Index));
+
+            return View(user);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HelloWebApp/HelloWebApp/Models/UserViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloWebApp.Models
+{
+    public class UserViewModel
+    {
+        public string? Name { get; set; }
+    }
+}
